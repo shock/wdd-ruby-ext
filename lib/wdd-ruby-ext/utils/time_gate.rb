0d4663c6e6a05353472b4882d72b80ca041739b1
shock <billdoughty@capitalthought.com>
2009-11-16 16:19:39 -0500

    Added elapsed time utility.  Scoped Utilities with WDDUtilities module.

--- lib/wdd-ruby-ext/utils/time_gate.rb
+++ lib/wdd-ruby-ext/utils/time_gate.rb
@@ -1,22 +1,30 @@
 # This is a time based throttler, with class scope.
 # When set, the waitTimeGate function will block or return true until the period set has elapsed
-class TimeGate
-  def set seconds
-    @gate_time = Time.now + seconds
-  end
+module WDDUtilities
+  class TimeGate
+    def set seconds
+      @gate_time = Time.now + seconds
+    end
   
-  def wait block = true
-    if block     
-      while Time.now < @gate_time do
-        sleep 0.2
+    def wait block = true
+      if block     
+        while Time.now < @gate_time do
+          sleep 0.2
+        end
+      else
+        Time.now < @gate_time
       end
-    else
-      Time.now < @gate_time
+    end
+  
+    def initialize
+      @gate_time = Time.now
     end
   end
   
-  def initialize
-    @gate_time = Time.now
+  def self.elapsed_time &block
+    start_time = Time.now
+    yield
+    Time.now - start_time
   end
 end
 
