5d5ef09f18974fa384bb3132cc2521953bb53516
shock <billdoughty@capitalthought.com>
2009-11-17 17:21:28 -0500

    Version bump to 0.0.4

--- lib/wdd-ruby-ext/utils/simpledebug.rb
+++ lib/wdd-ruby-ext/utils/simpledebug.rb
@@ -1,49 +1,50 @@
 # require 'pp'
 
-module WDD::SimpleDebug
+module WDD
+  module SimpleDebug
   
-  public
-  @__debugLevel = -1
-  @__prefix = ""
+    public
+    @__debugLevel = -1
+    @__prefix = ""
   
-  def setDebugLevel level
-    @__debugLevel = level
-  end
-  
-  def setDebugPrefix __prefix
-    @__prefix = __prefix
-  end
-  
-  def debugPrefix
-    @__prefix || ""
-  end
-  
-  def set_debug_level level
-    @__debugLevel = level
-  end
-
-  def __debugLevel
-    return @__debugLevel
-  end
-  
-  def debugLevel
-    @__debugLevel
-  end
-
-  def debug_msg message, level=0
-    @__debugLevel ||= -1
-    if( level <= @__debugLevel )
-      puts debugPrefix + message
+    def setDebugLevel level
+      @__debugLevel = level
     end
-  end
-
-  def show_msg message
-      puts debugPrefix + message
-  end
-
-  def error_msg message, level=0
-      puts debugPrefix + "ERROR: " + message
-  end
   
-end
+    def setDebugPrefix __prefix
+      @__prefix = __prefix
+    end
+  
+    def debugPrefix
+      @__prefix || ""
+    end
+  
+    def set_debug_level level
+      @__debugLevel = level
+    end
 
+    def __debugLevel
+      return @__debugLevel
+    end
+  
+    def debugLevel
+      @__debugLevel
+    end
+
+    def debug_msg message, level=0
+      @__debugLevel ||= -1
+      if( level <= @__debugLevel )
+        puts debugPrefix + message
+      end
+    end
+
+    def show_msg message
+        puts debugPrefix + message
+    end
+
+    def error_msg message, level=0
+        puts debugPrefix + "ERROR: " + message
+    end
+  
+  end
+end
--- lib/wdd-ruby-ext/utils/time_gate.rb
+++ lib/wdd-ruby-ext/utils/time_gate.rb
@@ -1,30 +1,31 @@
 # This is a time based throttler, with class scope.
 # When set, the waitTimeGate function will block or return true until the period set has elapsed
-module WDD::Utilities
-  class TimeGate
-    def set seconds
-      @gate_time = Time.now + seconds
-    end
+module WDD
+  module Utilities
+    class TimeGate
+      def set seconds
+        @gate_time = Time.now + seconds
+      end
   
-    def wait block = true
-      if block     
-        while Time.now < @gate_time do
-          sleep 0.2
+      def wait block = true
+        if block     
+          while Time.now < @gate_time do
+            sleep 0.2
+          end
+        else
+          Time.now < @gate_time
         end
-      else
-        Time.now < @gate_time
+      end
+  
+      def initialize
+        @gate_time = Time.now
       end
     end
   
-    def initialize
-      @gate_time = Time.now
+    def self.elapsed_time &block
+      start_time = Time.now
+      yield
+      Time.now - start_time
     end
   end
-  
-  def self.elapsed_time &block
-    start_time = Time.now
-    yield
-    Time.now - start_time
-  end
 end
-
