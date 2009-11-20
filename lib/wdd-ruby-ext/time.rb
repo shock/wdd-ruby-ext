03cec10ba1f68b285563ffb448658579ba2a6541
shock <billdoughty@capitalthought.com>
2009-11-20 11:30:05 -0500

    Added dbf method to Time classes for rendering database string formats.

--- lib/wdd-ruby-ext/time.rb
+++ lib/wdd-ruby-ext/time.rb
@@ -1,29 +1,47 @@
 
 class DateTime
   LOG_TIME_FORMAT = "%a %Y-%m-%d %H:%M:%S"
+  DB_TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
   
   # Format date and time for log timestamp
   def ltf
     strftime(LOG_TIME_FORMAT)
   end
+
+  # Format date and time for SQL
+  def dbf
+    strftime(DB_TIME_FORMAT)
+  end
 end
 
 class Time
   LOG_TIME_FORMAT = "%a %Y-%m-%d %H:%M:%S"
+  DB_TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
   
   # Format date and time for log timestamp
   def ltf
     strftime(LOG_TIME_FORMAT)
+  end
+
+  # Format date and time for SQL
+  def dbf
+    strftime(DB_TIME_FORMAT)
   end
 end
 
 if defined? ActiveSupport::TimeWithZone
   class ActiveSupport::TimeWithZone
     LOG_TIME_FORMAT = "%a %Y-%m-%d %H:%M:%S"
+    DB_TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
   
     # Format date and time for log timestamp
     def ltf
       strftime(LOG_TIME_FORMAT)
+    end
+
+    # Format date and time for SQL
+    def dbf
+      strftime(DB_TIME_FORMAT)
     end
   end
 end
