467d1e6a3ad8b1a2e1679c8ea6fddd261f516712
shock <billdoughty@capitalthought.com>
2010-11-08 11:35:50 -0600

    Removed TimeGate.wait_while_true method, and added an optional block to TimeGate.wait

--- lib/wdd-ruby-ext/utils/time_gate.rb
+++ lib/wdd-ruby-ext/utils/time_gate.rb
@@ -1,5 +1,10 @@
-# This is a time based throttler, with class scope.
-# When set, the waitTimeGate function will block or return true until the period set has elapsed
+# TimeGate is a time-based latch.
+# When intialized and set with a time period, +wait+ can be called to block the current thread's execution
+# until the time period has elapsed.
+#
+# The idea is that once set, the thread can go about doing other work, then at some point
+# wait on the gate to ensure that a certain amount of time has passed before continuing.
+# This is specifically useful for throttling API calls.
 module WDD
   module Utilities
     class TimeGate
@@ -7,19 +12,19 @@ module WDD
         @gate_time = Time.now + seconds
       end
   
+      # Waits until the gate is opened.
+      # +block+ - true by default.  If false, the method will not block, but will return the amount of time
+      # left before the gate opens.
+      # An optional Ruby block can be passed when +block+ is true that will be evaluated
+      # every 0.2 seconds.  If the block evaluates to false, the latch is released and this method returns.
       def wait block = true
-        if block     
-          while Time.now < @gate_time do
+        if block
+          guard = block_given? ? yield : true
+          while Time.now < @gate_time && guard do
             sleep 0.2
           end
         else
           Time.now < @gate_time
-        end
-      end
-  
-      def wait_while_true guard = Proc.new{true}
-        while Time.now < @gate_time && guard.call do
-          sleep 0.2
         end
       end
   
