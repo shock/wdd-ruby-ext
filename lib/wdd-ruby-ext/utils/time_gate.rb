86d950695a9675584be89b4a3da4c0b07c5e9332
shock <billdoughty@capitalthought.com>
2011-01-16 10:28:57 -0600

    Added interruptable sleep class method to TimeGate.

--- lib/wdd-ruby-ext/utils/time_gate.rb
+++ lib/wdd-ruby-ext/utils/time_gate.rb
@@ -16,12 +16,12 @@ module WDD
       # +block+ - true by default.  If false, the method will not block, but will return the amount of time
       # left before the gate opens.
       # An optional Ruby block can be passed when +block+ is true that will be evaluated
-      # every 0.2 seconds.  If the block evaluates to false, the latch is released and this method returns.
+      # every 0.5 seconds.  If the block evaluates to false, the latch is released and this method returns.
       def wait block = true
         if block
           guard = block_given? ? yield : true
           while Time.now < @gate_time && guard do
-            sleep 0.2
+            sleep 0.5
           end
         else
           Time.now < @gate_time
@@ -33,10 +33,26 @@ module WDD
       end
     end
   
-    def self.elapsed_time &block
-      start_time = Time.now
-      yield
-      Time.now - start_time
+    class << self
+      # Evaluated the passed block and returns the elapsed wall clock time.
+      def elapsed_time &block
+        start_time = Time.now
+        yield
+        Time.now - start_time
+      end
+      
+      # Sleeps the specified interval.  If a block is passed, it will be evaluated every +guard_interval+ seconds.
+      # If it evaluates to true, the sleep will be interrupted and control will be returned to the caller.
+      def interruptable_sleep sleep_time, guard_interval=0.5
+        if block_given?
+          while( yield && sleep_time > 0 ) 
+            sleep guard_interval
+            sleep_time -= guard_interval
+          end
+        else
+          sleep sleep_time
+        end
+      end
     end
   end
 end
