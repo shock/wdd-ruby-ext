bed773e7ed28679b3f3f7aafe274b65cd5fb3b63
shock <billdoughty@capitalthought.com>
2011-02-13 08:11:57 -0600

    Added TestServer, a test HTTP server.

--- lib/wdd-ruby-ext/utils.rb
+++ lib/wdd-ruby-ext/utils.rb
@@ -2,6 +2,8 @@
 
 require 'wdd-ruby-ext/utils/time_gate'
 require 'wdd-ruby-ext/utils/simpledebug'
+require 'wdd-ruby-ext/utils/hash_object'
 require 'wdd-ruby-ext/utils/helpers'
 require 'wdd-ruby-ext/utils/fixed_point'
 require 'wdd-ruby-ext/utils/pretty_xml'
+require 'wdd-ruby-ext/utils/test_server'
